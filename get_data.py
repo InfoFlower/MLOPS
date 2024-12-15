33807b14a7628a1081407e6d60622b467186c883
InfoFlower <gabriel.lebovic@gmail.com>
2024-12-15 12:28:44 +0100

    reset

--- get_data.py
+++ get_data.py
@@ -2,13 +2,13 @@ import logging
 import api_call as dc
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s', level=logging.DEBUG)
 import json
+import requests as rq
 
 def proc_get_data():
     logging.info('START GET DATA')
-    api_calleur=dc.get_api()
     url='https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/gbfs.json'
     logging.info(f'CALLING {url}')
-    data=api_calleur(url)
+    data=rq.get(url)
     file_path=f'data/.sys/gbfs.json'
     file_load=open(file_path,'w')
     file_load.write(data.text)
@@ -24,5 +24,5 @@ def proc_get_data():
             name=f"{i['name']}_{available_data['lastUpdatedOther']}"
             file_writer=open(f'data/{name}.json','w')
         logging.info(f'CALLING { i["url"] }')
-        file_writer.write(api_calleur(i['url']).text)
+        file_writer.write(rq.get(i['url']).text)
         file_writer.close()
