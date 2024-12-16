d3dca464f9a83591cc4a9af08fa6ad09b489ee88
InfoFlower <gabriel.jacometlebovic.etu@univ-lille.fr>
2024-12-16 17:59:49 +0100

    changement arborescence pour code plus quali

--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 
+
+
 def predict_model(model,data):
     return model.predict(data)
     
@@ -14,7 +16,12 @@ def train_model(model,data):
 def test_models(data_test
                 ,new_model
                 ,OG_model):
-    
+    results='champion'
+    score_challenger = scoring(new_model,data_test)
+    score_champion = scoring(OG_model,data_test)
+    if score_challenger>score_champion : 
+        results='challenger'
+    return results
     
 
 def score(X_train,X_test,y_train,y_test,model):
@@ -32,12 +39,12 @@ def score_cv(X_train,X_test,y_train,y_test,trained_model):
 
 
 def all_calc(df,
-             models=[(DecisionTreeClassifier(),'Arbre de décision'),(LogisticRegression(),'Logistique')],
+             model,
+             cols,
              top_cv=False,
              test_size=0.33,
              rnd_state=11,
              new_data=None,
-             cols=['workclass', 'education', 'marital-status', 'occupation','relationship', 'race', 'gender','native-country', 'income'],
              plot=False):
     """
     Segmente le jeu de données en train test size si besoin
@@ -47,19 +54,19 @@ def all_calc(df,
     X=df[df.columns[:-2]]
     y=df['income_>50K']
     if new_data is None:
+        logging.info('CREATION TRAIN TEST DATASETS')
         X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=test_size,random_state=rnd_state)
     else:
         X_train,y_train=(X,y)
     ret={}
-    for i in models:
-        i[0].fit(X_train,y_train)
-        if new_data is None:
-            if top_cv==True:
-                ret[i[1]]=score_cv(X_train,X_test,y_train,y_test,i[0])
-            else:
-                ret[i[1]]=score(X_train,X_test,y_train,y_test,i[0])
-        if new_data is not None:
-            ret[i[1]]=[i[0].predict(pd.get_dummies(new_data,columns=cols[:-1])),i[1]]
-        if plot == True and i[1]=='Arbre de décision':
-            plot_tree(i[0])
+    logging.info('FITTING DATA')
+    model.fit(X_train,y_train)
+    if new_data is None:
+        logging.info('START SCORING')
+        if top_cv==True:
+            ret[i[1]]=score_cv(X_train,X_test,y_train,y_test,i[0])
+        else:
+            ret[i[1]]=score(X_train,X_test,y_train,y_test,i[0])
+    if new_data is not None:
+        ret[i[1]]=[i[0].predict(pl.get_dummies(new_data,columns=cols[:-1])),i[1]]
     return ret
